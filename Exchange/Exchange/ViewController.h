9b592004e9dc613f711456ba70e15776debe107e
mesird <holy_mesird@126.com>
2015-08-23 23:58:00 +0800

    create the iOS project with core data

---
+++ Exchange/Exchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exchange
+//
+//  Created by mesird on 8/23/15.
+//  Copyright (c) 2015 mesird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Exchange/ExchangeTests/ExchangeTests.m
@@ -0,0 +1,40 @@
+//
+//  ExchangeTests.m
+//  ExchangeTests
+//
+//  Created by mesird on 8/23/15.
+//  Copyright (c) 2015 mesird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ExchangeTests : XCTestCase
+
+@end
+
+@implementation ExchangeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
